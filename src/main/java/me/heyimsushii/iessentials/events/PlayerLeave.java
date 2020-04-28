c0801639e0577c1f290263ecf319e4b7c822b916
HeyImSushii <ludviksoerensen@gmail.com>
2020-04-28 11:23:51 +0200

    Fixed issue where the custom quit message wasn't showing.

--- src/main/java/me/heyimsushii/iessentials/events/PlayerLeave.java
+++ src/main/java/me/heyimsushii/iessentials/events/PlayerLeave.java
@@ -1,6 +1,7 @@
 package me.heyimsushii.iessentials.events;
 
 import me.heyimsushii.iessentials.IEssentials;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,7 +13,9 @@ public class PlayerLeave implements Listener {
     public void onLeave(PlayerQuitEvent event) {
         Player player = event.getPlayer();
 
-        if (IEssentials.getConfigFile().getConfig().getString("enable-custom-quit-message").equals(true))
-            event.setQuitMessage(IEssentials.getConfigFile().getConfig().getString("custom-quit-message").replace("{PLAYERNAME}", player.getName()));
+        if (IEssentials.getConfigFile().getConfig().getBoolean("enable-custom-quit-message")) {
+            event.setQuitMessage(ChatColor.translateAlternateColorCodes('&', IEssentials.getConfigFile().getConfig().getString("custom-quit-message")
+                    .replace("{PLAYERNAME}", player.getName())));
+        }
     }
 }
